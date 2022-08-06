021353f2572ccb10cf40d23f3ee7ead4d2d9622e
Kammela Aradhan Babu <62125916+kammelaaradhan@users.noreply.github.com>
2022-08-06 12:27:53 +0530

    Add files via upload
    Python and it's programs using various modules and libraries as per difficulty-level
    
    Beginner Simple
    Intermediate Moderate
    Advanced High Level of python expertise require
    
    Source : From InterviewBit Reference Thanks for suggesting this projects for Python Developer

---
+++ Python/Python_and_its_apps/Intermediate_Level_programs/Fibonacci_Producer/Fibonacci-series-with-Python-master/fibonacci.py
@@ -0,0 +1,30 @@
+"""
+	Author: Edgard Diaz
+	Date: March 25th 2020
+	
+	In this code a recursive function is developed to 
+	generate the first n numbers of the Fibonacci series
+"""
+
+def  fibonacci(n):
+
+	if n == 1  or n == 0:
+
+		return n;
+
+	else:
+
+		return fibonacci(n-2) + fibonacci(n - 1)
+
+
+numero = int(input("ingrese un numero entero  positivo: "))
+
+if numero < 0:
+	print("Numero no valido")
+
+i = 0
+
+print("Secesion de fibonacci: ")
+
+for i in range(0, numero):
+	print(fibonacci(i))
